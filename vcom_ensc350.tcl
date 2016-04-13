c59f95198f670ef7e2c4b0896a6452517d68a064
Kurtis Bohlen <kbohlen@users.noreply.github.com>
2016-04-12 17:28:46 -0700

    Update vcom_ensc350.tcl

--- vcom_ensc350.tcl
+++ vcom_ensc350.tcl
@@ -1,10 +1,10 @@
 vlib work
 
-vcom H:/kurtis/Assignment2/ensc350_package.vhd
-vcom H:/kurtis/Assignment2/mem_control.vhd
-vcom H:/kurtis/Assignment2/pc_calc.vhd
-vcom H:/kurtis/Assignment2/instr_decode.vhd
-vcom H:/kurtis/Assignment2/rfile.vhd
-vcom H:/kurtis/Assignment2/full_alu.vhd
-vcom H:/kurtis/Assignment2/ensc350.vhd
-vcom H:/kurtis/Assignment2/tb_ensc350.vhd
+vcom ensc350_package.vhd
+vcom mem_control.vhd
+vcom pc_calc.vhd
+vcom instr_decode.vhd
+vcom rfile.vhd
+vcom full_alu.vhd
+vcom ensc350.vhd
+vcom tb_ensc350.vhd
