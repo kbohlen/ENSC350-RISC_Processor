02ebb808c802ad31f5ba247b76fe0a6f92b409fa
Kurtis Bohlen <kbohlen@users.noreply.github.com>
2016-04-12 17:27:48 -0700

    Add compilation file via upload

---
+++ vcom_ensc350.tcl
@@ -0,0 +1,10 @@
+vlib work
+
+vcom H:/kurtis/Assignment2/ensc350_package.vhd
+vcom H:/kurtis/Assignment2/mem_control.vhd
+vcom H:/kurtis/Assignment2/pc_calc.vhd
+vcom H:/kurtis/Assignment2/instr_decode.vhd
+vcom H:/kurtis/Assignment2/rfile.vhd
+vcom H:/kurtis/Assignment2/full_alu.vhd
+vcom H:/kurtis/Assignment2/ensc350.vhd
+vcom H:/kurtis/Assignment2/tb_ensc350.vhd
